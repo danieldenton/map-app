cbcfa33106ad091bb8b9486d3f1ab728a4b7274b
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-18 14:19:01 -0700

    the map is working

--- App.tsx
+++ App.tsx
@@ -1,30 +1,49 @@
 import React, { useState, useEffect } from "react";
-import MapView, { PROVIDER_GOOGLE, PROVIDER_DEFAULT } from "react-native-maps";
-import { StyleSheet, Platform, View } from "react-native";
+import MapView, {
+  Region,
+  PROVIDER_GOOGLE,
+  PROVIDER_DEFAULT,
+} from "react-native-maps";
+import { StyleSheet, Platform, View, ActivityIndicator } from "react-native";
 import * as Location from "expo-location";
 
 export default function App() {
   const googleMapFix =
     Platform.OS === "android" ? PROVIDER_GOOGLE : PROVIDER_DEFAULT;
-  const [location, setLocation] = useState({});
+  const [initialRegion, setInitialRegion] = useState<Region>();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     const getPermissions = async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
-        console.log("Please grant location permissions")
-        return
+        console.log("Please grant location permissions");
+        return;
       }
-      let currentLocation = await Location.getCurrentPositionAsync({})
-      setLocation(currentLocation)
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setInitialRegion({
+        latitude: currentLocation.coords.latitude,
+        longitude: currentLocation.coords.longitude,
+        latitudeDelta: 0.888,
+        longitudeDelta: 0.888,
+      });
     };
-    getPermissions()
-
+    getPermissions();
+    setIsLoading(false);
   }, []);
 
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} provider={googleMapFix} />
+      {isLoading ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        <MapView
+          style={styles.map}
+          provider={googleMapFix}
+          initialRegion={initialRegion}
+        />
+      )}
     </View>
   );
 }
@@ -32,9 +51,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   map: {
-    width: "100%",
-    height: "100%",
+    width: "90%",
+    height: "60%",
+   
+    borderStyle: "solid",
+    borderColor: "red",
+    borderWidth: 6,
+    borderRadius: 20,
   },
 });
