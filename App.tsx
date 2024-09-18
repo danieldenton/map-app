dc78c4f9406ff00d2686f4641ef471520bcf3509
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-17 18:42:23 -0700

    updated app.json/

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} provider={PROVIDER_GOOGLE} />
     </View>
   );
 }
@@ -14,8 +13,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
---
+++ _layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Stack } from "expo-router";
+
+const Layout = () => {
+  <Stack>
+    <Stack.Screen name="App" options={{ title: "Home" }} />
+  </Stack>
+};
+
+export default Layout
