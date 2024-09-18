ee0bae71fe562c8d6fb688298a0418e55d6542d6
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-09-17 19:51:48 -0700

    updated app.config.js

---
+++ app.config.js
@@ -0,0 +1,34 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: 'mapApp',
+    slug: 'mapApp',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.danieldentondev.mapapp',
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+    plugins: ['expo-router'],
+  },
+};
